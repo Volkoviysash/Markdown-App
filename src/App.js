1b0a8bc863a4fbbe5d49d086638bfc953c566895
volkovich <volkoviysash@gmail.com>
2023-03-03 01:47:36 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { marked } from "marked";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExpand } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
+  const [textEditor, setTextEditor] = useState("");
+  const [isEditorFull, setIsEditorFull] = useState(false);
+
+  const handleEditorChange = (event) => {
+    setTextEditor(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="editorBlock">
+        <div id="editorHeader" className="editorHeader">
+          Editor
+          <FontAwesomeIcon icon={faExpand} />
+        </div>
+        <textarea
+          id="editor"
+          className="editor"
+          onChange={handleEditorChange}
+        ></textarea>
+      </div>
+      <div className="previewBlock">
+        <div id="previewHeader" className="previewHeader">
+          Previewer
+          <FontAwesomeIcon icon={faExpand} />
+        </div>
+        <div
+          id="preview"
+          className="preview"
+          dangerouslySetInnerHTML={{ __html: marked(textEditor) }}
+        ></div>
+      </div>
     </div>
   );
 }
